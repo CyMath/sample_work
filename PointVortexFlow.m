7a4ae8ea54ca84017fd55249d8bd4163768d4aaf
CyMath <61561646+CyMath@users.noreply.github.com>
2020-07-30 13:11:55 -0600

    Add files via upload

---
+++ PointVortexFlow.m
@@ -0,0 +1,9 @@
+function [ complexVelocity ] = PointVortexFlow(gamma, origin, range)
+%POINTVORTEX Return complex (x + iy) velocity at all points in range for
+%   vortex with strength gamma with center origin.
+reframe = range - origin;
+
+reframe = reframe ./ (reframe .* conj(reframe)); % divide by square of modulus
+complexVelocity = reframe .* 1i .* gamma ./ (2 * pi);
+end
+
